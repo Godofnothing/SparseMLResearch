9eabcd57df2881ca97cfae21225b69b400acc229
blackadder <scope.denis@mail.ru>
2022-05-29 15:06:21 +0300

    Added deit mixed recipe and updated .sh

--- run_gradual_pruning.sh
+++ run_gradual_pruning.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
 
-NUM_PROC=2
+#SBATCH --job-name SparseML
+#SBATCH --partition gpu
+#SBATCH --nodes 1
+#SBATCH --gpus 4
+#SBATCH --cpus-per-task 16
+#SBATCH --time 24:00:00 # hh:mm:ss, walltime
+#SBATCH --mem 160G
+#SBATCH -w gn12
+
+source /home/${USER}/.bashrc
+source activate pysparse
+
+NUM_PROC=4
 MASTER_PORT=29501
 
 MODEL=deit_tiny_patch16_224 
-EXP=AC_DC_MFAC_300_ep_${MODEL}
+EXP=GPshort_GM_50_ep_${MODEL}_lr=1e-5
 
 python -m torch.distributed.launch \
     --nproc_per_node=${NUM_PROC} \
     --master_port=${MASTER_PORT} \
-    sparse_training_new.py \
+    sparse_training_old.py \
     \
-    /mnt/data/imagenet \
+    /gpfs/gpfs0/datasets/ImageNet/ILSVRC2012 \
     \
     --config configs/deit_old.yaml \
-    --sparseml-recipe recipes/gradual_pruning/vit_mfac_300_epochs.yaml \
+    --sparseml-recipe recipes/gradual_pruning_short/vit_gm_50_epochs_lr_max=1e-5.yaml \
     \
     --dataset imagenet \
     \
@@ -23,18 +35,18 @@ python -m torch.distributed.launch \
     \
     --experiment ${EXP} \
     \
-    -b 512 \
+    -b 256 \
     -vb 500 \
     --workers 16 \
     \
     --mfac-loader \
-    --mfac-batch-size 512 \
+    --mfac-batch-size 256 \
     \
     --log-sparsity \
     --log-wandb \
     \
     --amp \
     \
-    --checkpoint-freq 20 \
+    --checkpoint-freq 2 \
     --save-last \
     \
